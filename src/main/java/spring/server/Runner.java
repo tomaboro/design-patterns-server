ad4ff48ab59d51958843c6e64637beb667653b6a
Venioo <adamskitomasz844@gmail.com>
2018-02-01 18:27:15 +0100

    runner fix

--- src/main/java/spring/server/Runner.java
+++ src/main/java/spring/server/Runner.java
@@ -1,12 +1,22 @@
 package spring.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class Runner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Runner.class, args);
+	}
+
+	@Scheduled(fixedDelay = 1000)
+	public void scheduleFixedDelayTask() {
+		System.out.println(
+				"Fixed delay task - " + System.currentTimeMillis() / 1000);
 	}
 }
